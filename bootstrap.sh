3f5d75d94251e5af2a8519e3d969738996745f7b
Michal Tehnik <michal.tehnik@mictech.cz>
2014-11-14 06:57:08 +1100

    Add Bootstraping for clean Mac

---
+++ bootstrap.sh
@@ -0,0 +1,85 @@
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+binaries=(
+  rename
+  python
+  node
+  git
+)
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+brew cleanup
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# Apps
+apps=(
+  alfred
+  dropbox
+  google-chrome
+  iterm2
+  sublime-text3
+  atom
+  flux
+  sketch
+  skype
+  intellij-idea
+  clipmenu
+  mucommander
+  evernote
+  skitch
+  onepassword
+  viscosity
+  hipchat
+  fitbit-connect
+  bartender
+  kitematic
+  totalfinder
+  p4merge
+  gopro-studio
+  java7
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Setup Git
+git config --global user.name "Michal Tehnik"
+git config --global user.email michal.tehnik@mictech.cz
+
+GITHUB_KEY=~/.ssh/github_rsa
+SSH_CONFIG=~/.ssh/config
+
+if [ ! -f $GITHUB_KEY ]; then
+  ssh-keygen -t rsa -C "michal.tehnik@mictech.cz" -N '' -f $GITHUB_KEY
+  echo "Host GitHub github.com" >> $SSH_CONFIG
+  echo "Hostname github.com" >> $SSH_CONFIG
+  echo "IdentityFile $GITHUB_KEY" >> $SSH_CONFIG 
+  echo "\n" >> $SSH_CONFIG
+fi
