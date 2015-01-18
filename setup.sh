43dfc8131e5e7b5a5fc8c6103196655a16afe11d
Michal Tehnik <michal.tehnik@mictech.cz>
2015-01-18 20:36:49 +0100

    Remove obsolete script

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#Git
-ln -s ~/Dropbox/Config/dotfiles/.gitconfig ~/.gitconfig
-
-#P4merge
-ln -s ~/Dropbox/Config/p4merge/p4merge /usr/local/bin/p4merge
-ln -s ~/Dropbox/Config/p4merge/p4diff /usr/local/bin/p4diff
